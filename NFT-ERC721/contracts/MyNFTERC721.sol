c816786f5a3a9f1996456a6b5ab77d34ac0ed126
Subhajit Das <sd651846@gmail.com>
2022-03-29 19:36:23 +0530

    For listing on Opensea .added NFT images and json inside build . modified the MyNFTERC721 contract

--- NFT-ERC721/contracts/MyNFTERC721.sol
+++ NFT-ERC721/contracts/MyNFTERC721.sol
@@ -3,26 +3,35 @@
 pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol"; 
 
 
 contract MyNFTERC721 is ERC721Enumerable, Ownable{
 
+    using Strings for uint256;
+
+    //uint8 , maximum token can be mint at a time
+    uint8 private _maxTokenMintAtOneGo = 5;
+
     //to count the token ID
     uint256 private _tokenIdCounter;
 
+    string private _baseExtension = ".json";
 
-    //Mapping the token ID to baseURI
-    mapping(uint256 => string) private _baseURIs;
+
+    
 
     constructor(string memory _name, string memory _symbol) ERC721(_name,_symbol) {
-
+        _tokenIdCounter=1;
     }
-    function mint(string memory _tokenURI) public onlyOwner{
+    function mint(uint256 _tokenAmount) public onlyOwner{
+        require(_tokenAmount <= _maxTokenMintAtOneGo,"You can max mint 5 token at a time");
+        for(uint8 i=0;i< _tokenAmount;i++){
+            _safeMint(msg.sender,_tokenIdCounter);
+            _tokenIdCounter++;
+        }
         
-        require(keccak256(abi.encodePacked(_tokenURI)) !=  keccak256(abi.encodePacked("")),"Enter valid NFTURI");
-        _tokenIdCounter++;
-        _safeMint(msg.sender,_tokenIdCounter);
-        _baseURIs[_tokenIdCounter] = _tokenURI;
+        
 
     }
 
@@ -30,12 +39,12 @@ contract MyNFTERC721 is ERC721Enumerable, Ownable{
     function tokenURI(uint256 tokenId) public view  override returns (string memory) {
         require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
 
-        string memory baseURI = _baseURI(tokenId);
-        return bytes(baseURI).length > 0 ? baseURI : "";
+        string memory baseURI = _baseURI();
+        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString(), _baseExtension)) : "";
     }
 
-    function _baseURI(uint _tokenId) internal view returns (string memory) {
-        return _baseURIs[_tokenId];
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://QmQu1KZ16W2SrvwqCYMyJPg5JTAPJ1kwMj3QqMh4JEciYY/";
     }
 
     
